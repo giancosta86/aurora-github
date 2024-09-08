96c0e9fa6901c72481cd81b72bfa8e2f22eaa8b7
Gianluca Costa <gianluca@gianlucacosta.info>
2024-09-08 04:51:38 +0200

    Introduce rust-wasm test project

---
+++ tests/rust-wasm/src/lib.rs
@@ -0,0 +1,18 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn add_ninety(x: i32) -> i32 {
+    x + 90
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn unit_test_add_ninety() {
+        let result = add_ninety(8);
+
+        assert_eq!(result, 98);
+    }
+}
