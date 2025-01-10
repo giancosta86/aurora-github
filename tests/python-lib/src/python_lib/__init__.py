0f672753bdb202b0ed5fae6a7d31e684b01b2b43
Gianluca Costa <gianluca@gianlucacosta.info>
2025-01-10 18:29:25 +0100

    Introduce the `python-lib` test project

---
+++ tests/python-lib/src/python_lib/__init__.py
@@ -0,0 +1,13 @@
+def hello() -> str:
+    """
+    Returns a welcome string.
+    """
+
+    return "Hello from 🐍Python!"
+
+
+def my_sum(a: int, b: int) -> int:
+    """
+    Just sums two integers.
+    """
+    return a + b
