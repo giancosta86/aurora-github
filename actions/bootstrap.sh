003db92a7aea31cc938beea63edef360eb4ec3e0
Gianluca Costa <gianluca@gianlucacosta.info>
2026-05-30 13:14:48 +0200

    Vastly simplify the Bash bootstrap script

--- actions/bootstrap.sh
+++ actions/bootstrap.sh
@@ -2,19 +2,6 @@ set -e
 set -u
 set -o pipefail
 
-tracingVarName='AURORA_GITHUB_TRACING_ENABLED'
-
-requireInput() {
-  local name="$1"
-  local value="$2"
-
-  if [[ -z "$value" ]]
-  then
-    echo "❌ Missing action input: '$name'!" >&2
-    exit 1
-  fi
-}
-
 writeEnv() {
   local name="$1"
   local value="$2"
@@ -22,52 +9,10 @@ writeEnv() {
   echo "$name=$value" >> $GITHUB_ENV
 }
 
-not() {
-  local value="$1"
-
-  if [[ "$value" == 'true' ]]
-  then
-    echo false
-  else
-    echo true
-  fi
-}
-
-setTracing() {
-  local enabled="$1"
-
-  export "$tracingVarName=$enabled"
-
-  writeEnv $tracingVarName "$enabled"
-}
-
-trace() {
-  if [[ "${!tracingVarName:-}" == "true" ]]
-  then
-    echo "$@"
-  fi
-}
-
-shouldInstallCommand() {
+commandExists() {
   local command="$1"
-  local skipIfExisting="$2"
 
-  if type "$command" > /dev/null 2>&1
-  then
-    trace "🌟The '$command' command is already installed on the system!"
-
-    if [[ "$skipIfExisting" == "true" ]]
-    then
-      trace "💫Skipping the setup, as requested!"
-      return 1
-    else
-      trace "📥Still proceeding with the setup, as requested!"
-      return 0
-    fi
-  fi
-
-  trace "💭 The '$command' command is not available - now installing it..."
-  return 0
+  type "$command" > /dev/null 2>&1
 }
 
 installElvish() {
